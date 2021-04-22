ba7ff73ab628392cfa2945d82c100d05e4cba426
JuanPerales1961 <59516939+JuanPerales1961@users.noreply.github.com>
2021-04-22 00:38:28 -0500

    Update app.py

--- proyecto/app.py
+++ proyecto/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 app = Flask('__name__')
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = 'localhoskkkt'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'flaskcontacts'
